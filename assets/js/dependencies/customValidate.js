c6a56037695bebc1b4b91883a2db2cc4cb5ccca9
Maketroli <maketroli@gmail.com>
2014-11-21 01:01:32 -0600

    2nd commit

---
+++ assets/js/dependencies/customValidate.js
@@ -0,0 +1,26 @@
+(function(){
+
+	$('#sign-up-form').validate({
+		rules: {
+			name: {
+				required: true
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			password: {
+				minlength: 6,
+				required: true
+			},
+			confirmation: {
+				minlength: 6,
+				equalTo: '#password'
+			}
+		},
+		success: function(element) {
+			element.text('Ok!').addClass('valid');
+		}
+	});
+
+})();
